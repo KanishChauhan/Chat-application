6198e03027e45660dfabaf07fe07770394ec43f6
kanish chauhan <kanishchauhan959@gmail.com>
2023-01-29 23:33:55 +0530

    server side msg and client side msg aligned

--- public/client.js
+++ public/client.js
@@ -1,13 +1,23 @@
 let bt=document.getElementById('bt')
 bt.addEventListener('click',fun)
+bt.addEventListener('keypress',(event)=>{
+    
+        if (event.key === "Enter") {
+            // event.preventDefault();
+            fun()
+        }
+         
+})
+
 
 function fun(){
     var msg=document.getElementById('msg').value
     let div=document.createElement('div')
     div.innerHTML=`<p>${msg}</p>`
-    div.classList.add('incomingmsg')
-    let msgarea=document.getElementById('msgarea')
+    div.classList.add('outgoingmsg')
+    let msgarea=document.getElementById('outg')
     msgarea.appendChild(div)
+    document.getElementById('msg').value=''
     console.log(msg)
 
 }
